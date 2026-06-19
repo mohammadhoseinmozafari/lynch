e9461914ab82ca5c2b82185522391d0f4b910c52
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-06-19 17:53:00 -0400

    added: ColumnMissingness , RowsMissingness , MissingnessDistribution observation collectors

--- core/observation/collector.py
+++ core/observation/collector.py
@@ -1,18 +1,24 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import List
+from enum import Enum
+from typing import Any, List
+import uuid
 from core.observation.observation import Observation
 from core.observation.type import ObservationType
 
+class ObservationCollectorMethod(str, Enum):
+    NULL_RATE = "null_rate"
 class ObservationCollector(ABC):
-
+    id: str
     observation_type : ObservationType
-    method_name : str
+    method_name : ObservationCollectorMethod
+    
+    def __init__(self) -> None:
 
+        super().__init__()
+        self.id = f"{self.__class__.__name__}_{uuid.uuid4()}"
+        
     @abstractmethod
     def collect (self, context) -> List[Observation]:
         raise NotImplementedError
     
-    @abstractmethod
-    def build_observation (self, Any) -> Observation:
-        raise NotImplementedError
---
+++ core/observation/collectors/missing_rate.py
@@ -0,0 +1,156 @@
+from __future__ import annotations
+
+
+from typing import List
+
+import pandas as pd
+
+from core.observation.collector import (
+    ObservationCollector,
+    ObservationCollectorMethod
+)
+from core.observation.observation import Observation
+from core.observation.type import ObservationType
+from data_investigation.profile.missing_values.profiler import (
+    ColumnMissingRateProfile, 
+    MissingRateProfiler, 
+    MissingnessDistributionProfile, 
+    MissingnessDistributionProfile, 
+    RowsMissingRateProfile, 
+    RowsMissingRateProfile
+)
+
+
+
+
+
+
+
+
+class ColumnMissingnessObservationCollector(ObservationCollector):
+    
+    def __init__(self, profiler: MissingRateProfiler) -> None:
+        super().__init__() 
+        
+        self.observation_type = ObservationType.COLUMN_MISSINGNESS
+        self.method_name = ObservationCollectorMethod.NULL_RATE
+
+        self._profiler = profiler
+
+    
+    def collect (self, context) -> List[Observation]:
+        
+        df : pd.DataFrame = context.dataset
+        
+        observations : List[Observation] = []
+        
+        missingness_profile= self._profiler.profile_columns(df)
+
+        for column, profile in missingness_profile.items():
+            missingness_observation = self.build_observation(profile)
+
+            observations.append(missingness_observation)
+
+        
+        
+        
+        
+
+
+
+        return observations
+    
+
+        
+    def build_observation (self, profile :  ColumnMissingRateProfile) -> Observation:
+
+        return Observation(
+            type = ObservationType.COLUMN_MISSINGNESS,
+            payload = profile.to_dict(),
+            reliability= 1.0, 
+            collector_id= self.id
+        )
+
+    
+    
+
+class RowsMissingnessObservationCollector (ObservationCollector) :
+
+    def __init__(self, profiler: MissingRateProfiler) -> None:
+        super().__init__() 
+        
+        self.observation_type = ObservationType.ROWS_MISSINGNESS
+        self.method_name = ObservationCollectorMethod.NULL_RATE
+
+        self._profiler = profiler
+
+    def collect (self , context) -> List[Observation]:
+        
+        df = context.dataset
+        sample_size = context.sample_size
+
+        observations : List[Observation] = []
+
+        missingness_profile = self._profiler.profile_rows(df, sample_size)
+
+        missingness_observation = self.build_observation(missingness_profile)
+
+        observations.append(missingness_observation)
+        
+        return observations
+    
+    def build_observation (self , profile : RowsMissingRateProfile ) -> Observation:
+        
+        
+        
+
+        return Observation (
+            type = self.observation_type,
+            payload = profile.to_dict(),
+            reliability= 1.0,
+            collector_id= self.id
+
+        )
+
+class MissingnessDistributionObservationCollector (ObservationCollector) :
+
+    def __init__(self, profiler: MissingRateProfiler) -> None:
+        super().__init__() 
+        
+        self.observation_type = ObservationType.MISSINGNESS_DISTRIBUTION
+        self.method_name = ObservationCollectorMethod.NULL_RATE
+
+        self._profiler = profiler
+    
+    def collect (self, context) -> List[Observation]:
+        df = context.dataset
+
+        observations : List[Observation] = []
+
+        missingness_distribution_profile = self._profiler.profile_distribution(df)
+
+        distribution_evidence = self.build_observation (missingness_distribution_profile)
+
+        observations.append(distribution_evidence)
+        
+        return observations
+    
+    def build_observation (self , profile : MissingnessDistributionProfile ) -> Observation:
+        
+        
+        
+
+        return Observation (
+            type = self.observation_type,
+            payload = profile.to_dict(),
+            reliability= 1.0,
+            collector_id= self.id
+            )
+
+
+
+
+
+
+
+
