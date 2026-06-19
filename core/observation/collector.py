3a54f479d7995274092e69f8dd20ba90becdd952
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-06-19 16:46:20 -0400

    refined: base ObservationCollector

--- core/observation/collector.py
+++ core/observation/collector.py
@@ -2,9 +2,17 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List
 from core.observation.observation import Observation
+from core.observation.type import ObservationType
 
 class ObservationCollector(ABC):
 
+    observation_type : ObservationType
+    method_name : str
+
     @abstractmethod
     def collect (self, context) -> List[Observation]:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def build_observation (self, Any) -> Observation:
         raise NotImplementedError
