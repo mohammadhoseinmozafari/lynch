aa6cef5844d20fdccd8df7986fcbd7ef2c5978a5
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-06-20 14:25:28 -0400

    added : abstract class SignalExtractor

---
+++ core/signal/extractor.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from abc import  ABC, abstractmethod
+
+from typing import List
+import uuid
+
+from core.observation.observation import Observation
+from core.observation.type import ObservationType
+from core.signal.signal import Signal
+from core.signal.type import SignalType
+
+class SignalExtractor(ABC):
+
+
+    id: str
+    signal_type : SignalType
+    supporting_type : ObservationType
+    
+    def __init__(self) -> None:
+
+        super().__init__()
+        self.id = f"{self.__class__.__name__}_{uuid.uuid4()}"
+        
+    @abstractmethod
+    def collect (self, observations : List[Observation]) -> List[Signal]:
+        raise NotImplementedError
+    
