22bd790540e434b6e1aa5cee79445578a932cb96
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-06-20 10:39:12 -0400

    added abstract base class EvidenceCollector

---
+++ core/evidence/collector.py
@@ -0,0 +1,20 @@
+from abc import ABC , abstractmethod
+from typing import List
+import uuid
+
+from core.evidence.evidence import Evidence
+from core.evidence.evidence_type import EvidenceType
+class EvidenceCollector(ABC):
+
+    id: str
+    observation_type : EvidenceType
+    
+    def __init__(self) -> None:
+
+        super().__init__()
+        self.id = f"{self.__class__.__name__}_{uuid.uuid4()}"
+        
+    @abstractmethod
+    def collect (self, context) -> List[Evidence]:
+        raise NotImplementedError
+    
