cc08328ca9a40bd7c7672a01d788723c0f536621
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-06-22 09:55:05 -0400

    added : Abstract HealthSignalExtractor

--- core/signal/extractors/base.py
+++ core/signal/extractors/base.py
@@ -7,7 +7,9 @@ from ulid import ulid
 
 from core.observation.observation import Observation
 from core.observation.type import ObservationType
-from core.signal.signal import Signal
+from core.signal.base import Signal
+from core.signal.enums import SignalCategory
+from core.signal.health import HealthSignal
 from core.signal.type import SignalType
 
 class SignalExtractor(ABC):
@@ -22,6 +24,18 @@ class SignalExtractor(ABC):
         self.id = str(ulid())
         
     @abstractmethod
-    def extract (self, observations : List[Observation]) -> List[Signal]:
+    def extract (self, observations : List[Observation]) -> List:
         raise NotImplementedError
     
+
+class HealthSignalExtractor (SignalExtractor):
+    
+    def __init__(self) -> None:
+        super().__init__()
+        self.signal_category = SignalCategory.HEALTH
+    
+    @abstractmethod
+    def extract(self, observations: List[Observation]) -> List[HealthSignal]:
+        raise NotImplementedError
+
+
