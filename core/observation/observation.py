b8397a6a4197443ad5d60c4ae89ca2525c90a167
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-06-23 05:39:54 -0400

    added ObservationBatch

--- core/observation/observation.py
+++ core/observation/observation.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
+from dataclasses import dataclass
 from datetime import datetime
 from pydantic import Field, BaseModel
 from typing import Dict, Any
 from ulid import ulid
 from core.observation.type import ObservationType
-
+import pandas as pd
 class Observation(BaseModel):
     
     id : str = Field (default_factory= lambda : str(ulid()))
@@ -17,3 +18,6 @@ class Observation(BaseModel):
     collector_id : str
 
 
+@dataclass
+class ObservationBatch :
+    batch : pd.DataFrame 
