940fba4fc54972080e17ab5ee4ceb863659ea765
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-06-29 09:27:05 -0400

    profile -> modified: init , added: documentation

--- core/profile/profiler.py
+++ core/profile/profiler.py
@@ -2,18 +2,19 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from enum import Enum
 from typing import Any
 import pandas as pd
 
+from enum import Enum
 
 
 class ProfilerType (str, Enum) :
     DATASET = 'dataset'
     MODEL = 'model'
-
-
 class DatasetProfiler (ABC) :
+    """
+    Base abstract class for all dataset profilers
+    """
 
     id : str
     profiler_type :  ProfilerType = ProfilerType.DATASET
