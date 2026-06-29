b86b4eb783589e26067626c63059c7bdf71f5688
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-06-29 09:36:19 -0400

    added : id generation in init

--- core/profile/profiler.py
+++ core/profile/profiler.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Any
 import pandas as pd
-
+from ulid import ulid
 from enum import Enum
 
 
@@ -16,9 +16,10 @@ class DatasetProfiler (ABC) :
     Base abstract class for all dataset profilers
     """
 
-    id : str
     profiler_type :  ProfilerType = ProfilerType.DATASET
-
+    def __init__(self) -> None:
+        super().__init__()
+        self.id = str(ulid())
     
     @abstractmethod
     def profile (self, df : pd.DataFrame)  -> Any:
