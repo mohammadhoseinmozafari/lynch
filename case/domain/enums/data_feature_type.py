f1daa1b520b85e6bb1630551b88cbe38fd86c320
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-07-01 17:57:42 -0400

    modified : DatasetBinding, DataSchema

--- case/domain/enums/data_feature_type.py
+++ case/domain/enums/data_feature_type.py
@@ -3,6 +3,9 @@ from enum import Enum
 class DataFeatureType (str, Enum) :
     NUMERIC = "numeric"
     CATEGORICAL = "categorical"
-    #DATETIME = "datetime"
+    DATETIME = "datetime"
+    BOOLEAN = "boolean"
+    TEXT = "text"
+    UNKNOWN = "unknow"
     ## text_length
     ## geospatial
--- case/domain/value_objects/dataset_binding.py
+++ case/domain/value_objects/dataset_binding.py
@@ -3,21 +3,14 @@ from pydantic import BaseModel, ConfigDict, Field
 from typing import Dict, Optional, List
 from case.domain.enums.data_split_name import DataSplitName
 from case.domain.enums.task_type import TaskType
-from case.domain.value_objects.data_split import DataSplit
+from case.domain.value_objects.data_profile import DataProfile
+from case.domain.value_objects.data_schema import DataSchema
 class DatasetBinding(BaseModel) :
+
     dataset_name : Optional[str] = Field(None)
-    data_splits : Dict[DataSplitName, DataSplit] = Field(min_length=1)
+    dataset_schema : DataSchema
+    dataset_profile: DataProfile
     drift_baseline : Optional[DataSplitName] = Field(default=None)
     task_type : Optional[TaskType] = Field(default=None)
 
-
-
-    def get_split(self, split_name : str) -> Optional[DataSplit]:
-        enum_key = DataSplitName(split_name)
-        return self.data_splits.get(enum_key)
-    
-    def get_split_names(self) -> List[str]:
-        return [split_name.value for  split_name in self.data_splits.keys()]
-
-    
     model_config = ConfigDict(arbitrary_types_allowed=True)
